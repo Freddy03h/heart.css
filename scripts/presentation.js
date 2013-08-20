0e9c2a3486f123c76e7339dc10ee33420bf4c8e9
freddy <freddy03h@gmail.com>
2013-08-20 02:03:35 +0200

    page wiki and examples

---
+++ scripts/presentation.js
@@ -0,0 +1,1 @@
+"use strict";var phoneViewportButton=$(".button-vp-phone"),tabletteViewportButton=$(".button-vp-tablette"),bodyEl=$(document.body);phoneViewportButton.on("click",function(){bodyEl.removeClass("viewport-tablette")}),tabletteViewportButton.on("click",function(){bodyEl.addClass("viewport-tablette")});
